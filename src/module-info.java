9c63030e7d953bb6e1ca36e4ad23bb2216eb80b6
Kennerges <kennerges@gmail.com>
2020-09-07 14:06:47 -0300

    Adding database access

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module workshop.javafx.jdbc {
-    requires javafx.fxml;
-    requires javafx.controls;
-    opens gui;
-    opens application;
-    opens model.entities;
-    opens model.services;
-}
